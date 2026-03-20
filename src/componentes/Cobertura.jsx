f7f56954378158d99d4e915d6c019e0b5e0d9b57
LuisSebas060104 <113657225+LuisSebas060104@users.noreply.github.com>
2026-03-19 23:43:24 -0600

    Componente de cobertura en donde se define nuestro alcance de trabajo

---
+++ src/componentes/Cobertura.jsx
@@ -0,0 +1,13 @@
+export function Cobertura() {
+  return (
+    <section id="cobertura" style={{ textAlign: 'center' }}>
+      <h2>Nuestra Cobertura</h2>
+      <p>
+        Operamos en todas las ciudades principales del territorio nacional y contamos con alianzas 
+        estratégicas para cubrir toda Centroamérica, Norteamérica, Europa y Asia.
+      </p>
+    </section>
+  );
+}
+
+export default Cobertura;
